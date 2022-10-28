6c371215c9862910c253826afb063e9126206a38
Paragon <zacharynickel14@gmail.com>
2022-10-28 15:46:39 -0500

    first files

---
+++ src/main/java/net/copperfox/creaturecraftmod/CreatureCraftClient.java
@@ -0,0 +1,10 @@
+package net.copperfox.creaturecraftmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreatureCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/copperfox/creaturecraftmod/CreatureCraftMod.java
@@ -0,0 +1,16 @@
+package net.copperfox.creaturecraftmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreatureCraftMod implements ModInitializer {
+	//comment
+	public static final String MOD_ID = "creaturecraftmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
